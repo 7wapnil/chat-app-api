1f8b8c4ff107cdd9f2fd48250b282ed0c7619010
Swapnil Sable <swapnil.sable@outlook.com>
2021-01-18 00:40:31 +0530

    Added required models. Assigned required associations.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
+  has_many :participants
+  has_many :conversations, through: :participants
+  has_many :messages
 
   def full_name
     "#{first_name} #{last_name}"
